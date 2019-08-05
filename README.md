46d796d0a5bc6b728b95ad4a504788072e0a8fc3
Richa Mirashi <richamirashi24@gmail.com>
2019-08-04 22:53:46 -0700

    Added soil moisture stat device code

--- README.md
+++ README.md
@@ -3,10 +3,20 @@ Raspberry pi controller for smart watering system
 
 ## Raspberry pi setup
 
+### Install
+
 ```
 pip install AWSIoTPythonSDK
+pip install boto
+pip3 install adafruit-circuitpython-ads1x15
 ```
+
+### Enable I2c
+
+https://learn.sparkfun.com/tutorials/raspberry-pi-spi-and-i2c-tutorial/all
 
 ## Reference Links
 
 * https://github.com/aws/aws-iot-device-sdk-python
+* https://github.com/adafruit/Adafruit_CircuitPython_ADS1x15
+* https://learn.adafruit.com/adafruit-4-channel-adc-breakouts/python-circuitpython
--- device/driver.py
+++ device/driver.py
@@ -3,6 +3,11 @@ import json
 import time
 import logging
 
+import board
+import busio
+
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 import RPi.GPIO as GPIO
 
 PUMP_PIN = 12           # pin12
@@ -22,7 +27,25 @@ def waterPlants(plantPort, duration):
         destroy()
 
 def getSoilMoistureStat(plantPort):
-    return "7%"
+    # Create the I2C bus
+    i2c = busio.I2C(board.SCL, board.SDA)
+    # Create the ADC object using the I2C bus
+    ads = ADS.ADS1115(i2c)
+    # Create single-ended input on channel 0
+    chan = AnalogIn(ads, ADS.P0)
+    return getNormalizedMoisturePercentValue(chan.value)
+
+
+def getNormalizedMoisturePercentValue(value):
+    low = 9000 # for 100%
+    high = 21000 # for 0%
+    if value is None:
+        return 0;
+    if value > high:
+        return 0
+    if value <= low:
+        return 100
+    return  100 - ( ((float(value) - low ) / high) * 100 )
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
